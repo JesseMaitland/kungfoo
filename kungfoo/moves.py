41c1374615ff75b1dd1ff819a76dfd7ee89610c7
Jesse Maitland <jesse.maitland@heyjobs.de>
2022-11-24 13:01:08 +0100

    feat(moves): new moves ready for release

--- kungfoo/moves.py
+++ kungfoo/moves.py
@@ -5,3 +5,11 @@ def punch() -> None:
 
 def kick() -> None:
     print("roundhouse kick!")
+
+
+def knee() -> None:
+    print("dragon knee of power!")
+
+
+def threaten() -> None:
+    print("die scumbag!")
